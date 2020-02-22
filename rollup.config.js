f08bf4da56851775c8c4326c63cb2e2154abfbf8
Daniel Imfeld <daniel@danielimfeld.com>
2020-02-22 02:14:32 +0000

    Add babel, eslint, and update postcss extractor for class: syntax

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,34 @@
 import svelte from 'rollup-plugin-svelte';
 import resolve from '@rollup/plugin-node-resolve';
-import commonjs from 'rollup-plugin-commonjs';
+import commonjs from '@rollup/plugin-commonjs';
+import babel from 'rollup-plugin-babel';
 import livereload from 'rollup-plugin-livereload';
 import { terser } from 'rollup-plugin-terser';
 import postcss from 'rollup-plugin-postcss';
-import autoPreprocess from 'svelte-preprocess';
+const svelteConfig = require('./svelte.config');
 
 const production = !process.env.ROLLUP_WATCH;
+
+const babelConfig = {
+  extensions: ['.js', '.mjs', '.html', '.svelte', '.ts'],
+  exclude: ['node_modules/@babel/**'],
+  presets: [
+    [
+      '@babel/preset-env',
+      {
+        targets: { chrome: 78, firefox: 70 },
+      },
+    ],
+    '@babel/preset-typescript',
+  ],
+  plugins: [
+    '@babel/plugin-syntax-dynamic-import',
+    '@babel/proposal-class-properties',
+    '@babel/plugin-proposal-object-rest-spread',
+    '@babel/plugin-proposal-nullish-coalescing-operator',
+    '@babel/plugin-proposal-optional-chaining',
+  ],
+};
 
 export default {
   input: 'src/main.js',
@@ -18,9 +40,7 @@ export default {
   },
   plugins: [
     svelte({
-      preprocess: autoPreprocess({
-        postcss: true,
-      }),
+      preprocess: svelteConfig.preprocess,
       // enable run-time checks when not in production
       dev: !production,
       css: (css) => {
@@ -38,6 +58,7 @@ export default {
     // https://github.com/rollup/rollup-plugin-commonjs
     resolve({ browser: true }),
     commonjs(),
+    babel(babelConfig),
 
     // Watch the `public` directory and refresh the
     // browser on changes when not in production
